691d6348de208fa5319d97e93d520d75affde730
ImKartikey27 <kartikey.22b1531008@abes.ac.in>
2025-04-12 01:13:26 +0530

    Add createCandidate to candidate controller

--- src/controller/candidate.controllers.js
+++ src/controller/candidate.controllers.js
@@ -9,3 +9,29 @@ const getCandidates = asyncHandler(async (req, res) => {
     return res.status(200).json(new ApiResponse(200, candidates, "Candidates fetched successfully"))
 })
 
+const createCandidate = asyncHandler(async (req, res) => {
+    const {name , email , phone , notice_period , current_ctc , expected_ctc} = req.body
+    //validation 
+    if([name , email , phone , notice_period , current_ctc , expected_ctc].some((field) => field?.trim()=== "")){
+        throw new ApiError(400, "All fields are required")
+    }
+    const existedCandidate = await Candidate.findOne({
+        $or: [{email: email},{phone: phone}]
+    })
+    if(existedCandidate){
+        throw new ApiError(400, "Candidate already exists")
+    }
+    try {
+        const candidate = await Candidate.create({
+            name,
+            email,
+            phone,
+            notice_period,
+            current_ctc,
+            expected_ctc
+        })
+        return res.status(201).json(new ApiResponse(201, candidate, "Candidate created successfully"))
+    } catch (error) {
+        throw new ApiError(500, error.message)
+    }
+})
